0aa1804efe7aebd2727d330b3b2471efded2950e
Bury Huang <bury@deuniv.com>
2022-06-17 23:42:58 -0700

    update using aws cloud endpoint

--- index.js
+++ index.js
@@ -1,59 +1,70 @@
-//This param apiURL can be changed if needed
-const apiURL = 'http://159.89.28.183:5000/details?url='
-
-//When the function it's done, I'll display this message for a short time
+//Function to make the POST request and display the result message from server
 const displayTextAlert = () => {
 
-    let alertElement = `<div id='alertSent' 
-        style='position: fixed;
-            top: 0px;
-            width: 100%;
-            display:flex;
-            justify-content: center;
-            z-index:999;
-            background-color: #000000;'>
+    //This param apiURL can be changed if needed
+    const apiURL = 'https://7l0593rc6k.execute-api.us-east-1.amazonaws.com/details?url='
+
+    //POST request to server
+    const submitURL = async (value) => {
+        await fetch(apiURL+encodeURIComponent(value), {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+    
+            }
+        }).then(r => r.json())
+        .then(d => {
+            console.log(d)
+            let alertElement = `<div id='alertSent' 
+            style='position: fixed;
+                top: 0px;
+                width: 100%;
+                display:flex;
+                justify-content: center;
+                z-index:999;
+                background-color: #00FF00;'>
                     <h1 style='color: #FFF;'>PAGE SAVED</h1>
-        </div>`
-    document.body.innerHTML +=(alertElement)
-    setTimeout(() => {document.getElementById('alertSent').remove()}, 2000)
-  }
+            </div>`
+            document.body.innerHTML +=(alertElement)
+            setTimeout(() => {document.getElementById('alertSent').remove()}, 2000)
+        })
+        .catch (err => {
+            console.log(err)
+            let alertElement = `<div id='alertSent' 
+            style='position: fixed;
+                top: 0px;
+                width: 100%;
+                display:flex;
+                justify-content: center;
+                z-index:999;
+                background-color: #FF0000;'>
+                    <h1 style='color: #FFF;'>PAGE COULDN'T BE SENT</h1>
+            </div>`
+            document.body.innerHTML +=(alertElement)
+            setTimeout(() => {document.getElementById('alertSent').remove()}, 2000)
+        })
+    }
 
-//function to call the message alert
-const displayAlert = () => {
-    chrome.tabs.query( {active: true, currentWindow: true },
-        (tabs) =>
-        {
-            chrome.scripting.executeScript(
-                {
-                    target: {tabId: tabs[0].id},
-                    function: displayTextAlert
-                }
-            );
-        }
-    )
-    setTimeout(() => {
-    window.close()
-    }, 20); 
+    //Executing the function to send the URL
+    submitURL(window.location.href)
 }
 
-//function to send the actual tab to server
+//Main function (to execute the script into the active tab)
 const catchURL = () => {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        submitURL(tabs[0].url)
-        displayAlert()
+    chrome.tabs.query({active: true, currentWindow: true}, async (tabs) => {
+        chrome.scripting.executeScript(
+            {
+                target: {tabId: tabs[0].id},
+                function: displayTextAlert
+            }
+        );
     })
+
+    setTimeout(() => {
+        window.close()
+    }, 20);
 }
 
-//function to send the link to server
-const submitURL = async (value) => {
-    await fetch(apiURL+encodeURIComponent(value), {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-
-        }
-    }).catch (err => console.log(err))
-}
-
+//Call the main function
 catchURL()
